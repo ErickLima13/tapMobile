94d6bc9dcfb1241be670c29d1a2ea77549628d78
ErickAraujo <erickequipe7@gmail.com>
2025-12-03 10:29:20 -0300

    start implementation of sound system

--- Assets/_Project/Scripts/Runtime/Systems/Main Menu/MenuController.cs
+++ Assets/_Project/Scripts/Runtime/Systems/Main Menu/MenuController.cs
@@ -1,13 +1,22 @@
+using Maneuver.SoundSystem;
 using UnityEngine;
+using Zenject;
 
 public class MenuController : MonoBehaviour
 {
+    [Inject] private IAudioManager _audioManager;
+
+    [SerializeField] private AudioFileObject _musicMenu;
+
     private void Start()
     {
         Time.timeScale = 1;
+        _audioManager.Play(_musicMenu);
     }
 
 
-
-
+    public void StopMenuMusic()
+    {
+        _audioManager.Stop(_musicMenu);
+    }
 }
--- Assets/_Project/Scripts/Runtime/Systems/UI/SpawnFireEffect.cs
+++ Assets/_Project/Scripts/Runtime/Systems/UI/SpawnFireEffect.cs
@@ -1,8 +1,15 @@
+using Maneuver.SoundSystem;
 using System;
 using UnityEngine;
+using Zenject;
 
 public class SpawnFireEffect : MonoBehaviour
 {
+    [Inject] private IAudioManager _audioManager;
+
+    [SerializeField] private AudioFileObject _fireballVfx;
+
+
     public event Action<float> OnAnimation;
 
     private Animator _animator;
@@ -14,6 +21,7 @@ public class SpawnFireEffect : MonoBehaviour
 
     public void PlayAnimation(Vector3 position)
     {
+        _audioManager.Play(_fireballVfx);
         transform.position = position;
         _animator.Play("explosionTouch");
         OnAnimation?.Invoke(position.x);
