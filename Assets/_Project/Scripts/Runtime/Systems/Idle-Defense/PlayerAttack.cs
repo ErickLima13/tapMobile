c8e72e0b877b409fe355ef97c3613d38a7340506
ErickAraujo <erickequipe7@gmail.com>
2026-04-10 14:13:49 -0300

    coment script

--- Assets/_Project/Scripts/Runtime/Systems/Idle-Defense/PlayerAttack.cs
+++ Assets/_Project/Scripts/Runtime/Systems/Idle-Defense/PlayerAttack.cs
@@ -1,112 +1,100 @@
-using System;
-using System.Collections.Generic;
-using UnityEngine;
-using Zenject;
-using Random = UnityEngine.Random;
+//using System;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using Zenject;
+//using Random = UnityEngine.Random;
 
-public class PlayerAttack : MonoBehaviour, IPooledObject
-{
-    [Inject] private ObjectPooler _objectPooler;
-    [Inject] private PlayerStatus _playerStatus;
-    [Inject] private WaveController _waveController;
+//public class PlayerAttack : MonoBehaviour, IPooledObject
+//{
+//    [Inject] private ObjectPooler _objectPooler;
+//    [Inject] private PlayerStatus _playerStatus;
+//    //[Inject] private WaveController _waveController;
 
-    public event Action<PointType> OnAttackEvent;
+//    public event Action<PointType> OnAttackEvent;
 
-    public EnemyCollider _target;
+//    public EnemyCollider _target;
 
-    public PlayerAttributes _playerAttributes;
+//    public WeaponAttributes _playerAttributes;
 
-    private List<EnemyCollider> enemyColliders = new();
+//    private List<EnemyCollider> enemyColliders = new();
 
-    public void CheckArea()
-    {
-        if (_waveController.GetCurrentWave().Count > 0)
-        {
-            enemyColliders.AddRange(_waveController.GetCurrentWave());
-            int idRand = Random.Range(0, enemyColliders.Count);
-            _target = enemyColliders[idRand];
-            RotateTheEnemy();
-        }
-    }
-
-    private void Update()
-    {
-        _playerAttributes = _playerStatus.playerAttributes; // tirar quando fizer mecanica roguelike
-
-        if (_waveController.GetEnemiesInScene() == 0)
-        {
-            return;
-        }
-
-        if (_target == null || !_target.gameObject.activeSelf)
-        {
-            CheckArea();
-        }
-
-        Movement();
-    }
-
-    private void Movement()
-    {
-        if (_target != null)
-        {
-            Vector3 newTarget = Vector3.MoveTowards(transform.position, _target.gameObject.transform.position,
-                _playerAttributes.AttackSpeed * Time.deltaTime);
-            transform.position = newTarget;
-        }
-    }
-
-    private void RotateTheEnemy()
-    {
-        Vector3 direction = _target.transform.position - transform.position;
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg + 90;
-        Quaternion rotation = transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * 1);
-    }
+//    public EnemyRuntimeLookup enemyRuntimeLookup;
 
 
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.TryGetComponent<EnemyCollider>(out EnemyCollider enemy) == _target)
-        {
-            if (!enemy.GetIsDied())
-            {
-                enemy.CheckLife(enemy);
-                _objectPooler.ReturnToPool("playerAttack", gameObject);
-            }
-        }
-    }
+//    public void CheckArea()
+//    {
+//        if (enemyRuntimeLookup.CurrentEnemies.Count > 0)
+//        {
+//            enemyColliders.AddRange(enemyRuntimeLookup.CurrentEnemies);
+//            int idRand = Random.Range(0, enemyColliders.Count);
+//            _target = enemyColliders[idRand];
+//            RotateTheEnemy();
+//        }
+//    }
 
-    public void OnObjectSpawn()
-    {       
-    }
+//    private void Update()
+//    {
+//        _playerAttributes = _playerStatus.playerAttributes; // tirar quando fizer mecanica roguelike
 
-    private void OnDisable()
-    {
-        _target = null;
-        transform.position = new(0, -2, 0);
-    }
-}
+//        if (enemyRuntimeLookup.CurrentEnemies.Count == 0)
+//        {
+//            return;
+//        }
 
-[System.Serializable]
-public struct PlayerAttributes
-{
-    public float AttackSpeed;
-    public int AttackCount;
-    public float AttackTime;
+//        if (_target == null || !_target.gameObject.activeSelf)
+//        {
+//            CheckArea();
+//        }
 
-    public PlayerAttributes(float attackSpeed, int attackCount, float attackTime)
-    {
-        AttackSpeed = attackSpeed;
-        AttackCount = attackCount;
-        AttackTime = attackTime;
-    }
-}
+//        Movement();
+//    }
 
-public enum AttributesType
-{
-    AttackSpeed,
-    AttackCount,
-    TimeToAttack
-}
+//    private void Movement()
+//    {
+//        if (_target != null)
+//        {
+//            Vector3 newTarget = Vector3.MoveTowards(transform.position, _target.gameObject.transform.position,
+//                _playerAttributes.AttackSpeed * Time.deltaTime);
+//            transform.position = newTarget;
+//        }
+//    }
+
+//    private void RotateTheEnemy()
+//    {
+//        Vector3 direction = _target.transform.position - transform.position;
+//        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg + 90;
+//        Quaternion rotation = transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+//        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * 1);
+//    }
+
+
+//    private void OnTriggerEnter2D(Collider2D collision)
+//    {
+//        if (collision.TryGetComponent<EnemyCollider>(out EnemyCollider enemy) == _target)
+//        {
+//            if (!enemy.GetIsDied())
+//            {
+//                enemy.CheckLife(enemy);
+//                _objectPooler.ReturnToPool("playerAttack", gameObject);
+//            }
+//        }
+//    }
+
+//    public void OnObjectSpawn()
+//    {       
+//    }
+
+//    private void OnDisable()
+//    {
+//        _target = null;
+//        transform.position = new(0, -2, 0);
+//    }
+//}
+
+//public enum AttributesType
+//{
+//    AttackSpeed,
+//    AttackCount,
+//    TimeToAttack
+//}
 
